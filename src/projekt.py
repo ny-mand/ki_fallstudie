bb7f3877b7843b2dc920f33f26a52a5938ac64be
Ben <benschwarz14@gmx.de>
2025-11-13 17:02:31 +0100

    projekt klasse

--- src/projekt.py
+++ src/projekt.py
@@ -1,6 +1,7 @@
 class Projekt_Base:
-    def __init__(self, name):
+    def __init__(self, name, date_created):
         self.name = name
+        self.date_created = date_created
 
     def get_name(self):
         return self.name
